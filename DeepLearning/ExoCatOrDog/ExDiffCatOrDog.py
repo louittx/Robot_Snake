3c9340f4fba42264f4904edb84d470517f55ad69
louittx <louittx@gmail.com>
2025-07-08 19:08:39 +0200

    finish the code for the ExDeiffCatOrDog

--- DeepLearning/ExoCatOrDog/ExDiffCatOrDog.py
+++ DeepLearning/ExoCatOrDog/ExDiffCatOrDog.py
@@ -1,9 +1,32 @@
 #Le but de l'exo c'est avec un datasheet d'imge de diférencier un chient ou un chat sur une imaeg de 64px par 64px
 
+# TO DO
+# 1. Normaliser le train_set et le test_set (0-255 -> 0-1)
+# 2. aplatir les matrcie pour avoir 1000,4096 les variables du train_set et du test_set (64x64 -> 4096)
+# 3. Entraîner le modele sur le train_set (tracer la courbe d'apprentissage, trouver les bons hyper-params)
+#    (si vous rencontrez un probleme avec le log_loss, utiliser la fonction de sklearn a la place !)
+# 4. Évaluer le modele sur le test_set (tracer également la courbe de Loss pour le test_set)
+# 5. Partager vos conclusions dans les commentaires !
+
 from utilities import *
 from PerceptronLib import *
 
-XTrain, yTrain, Xtest, yTest = load_data()
+def Normaliser(X):
+    return((X-X.min())/(X.max()-X.min()))
 
-print(Xtest.shape)
-print(yTrain.shape)
+
+
+XTrain, yTrain, XTest, yTest = load_data()
+
+XTrainNorm = Normaliser(XTrain)
+XTestNorm = Normaliser(XTest)
+
+XTrainNormFlat = XTrainNorm.reshape(XTrainNorm.shape[0],-1) # redimensiont la matrix de 1000 64 64 en 1000 4096/ le -1 calculet automatiquemnt le nobre de collone restant
+XTestNormFlat = XTestNorm.reshape(XTestNorm.shape[0],-1)
+print(XTrain.shape)
+print(XTrainNormFlat.shape)
+
+Algo(XTrainNormFlat,yTrain,XTestNormFlat,yTest,a=0.01,i=10000)
+
+# Sur la courbe nous voyons que notre IA est en ovefiniti donc elle aprende trop pour rien, Pour resoudre cela nous pouvons rejouter plus d'image de chient et de chat,
+#cela va pas cahnger car notre IA est sur un seul neront donc lineaire
--- DeepLearning/ExoCatOrDog/PerceptronLib.py
+++ DeepLearning/ExoCatOrDog/PerceptronLib.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import log_loss
+from tqdm import tqdm
 
 
-def init(x):
+def init(X):
     W = np.random.randn(X.shape[1],1)
     b = np.random.randn(1)
     return (W,b)
@@ -15,7 +16,8 @@ def Model(X,W,b):
     return A
 
 def Cost(A,y) : 
-    return (-1/ len(y))*sum(y *np.log(A)+(1-y)*np.log(1-A))
+    #return (-1/ len(y))*sum(y *np.log(A)+(1-y)*np.log(1-A))
+    return log_loss(y,A)
 
 def Gradiants(A,X,y) :
     dW = (1/len(y))*np.dot(X.T,A-y)
@@ -29,5 +31,58 @@ def Uptate(W,b,dW,db, a) :
 
 def predict(x,W,b):
     A = Model(x,W,b)
-    print(A)
     return A>=0.5
+
+def Algo(X,y,a=0.1,i=100):
+    W,b = init(X)
+    LogLoss=[]
+    Acc = []
+    for i in tqdm(range(i)): # tqdm permet d'afficher un bare de progression
+        A = Model(X,W,b)
+        if (i%10 == 0): # permte d'aller plus rapitement
+            LogLoss.append(Cost(A,y))
+            yPredict = predict(X,W,b)
+            Acc.append(accuracy_score(y,yPredict))
+        dW,db = Gradiants(A,X,y)
+        W,b = Uptate(W,b,dW,db,a)
+
+    plt.figure(figsize=(12,4))
+    plt.subplot(1,2,1)
+    plt.plot(LogLoss)
+    plt.subplot(1,2,2)
+    plt.plot(Acc)
+    plt.show()
+    return (W,b)
+
+def Algo(XTrain,yTrain,XTest,yTest,a=0.1,i=100):
+    W,b = init(XTrain)
+    LogLossTrain=[]
+    AccTrain = []
+    LogLossTest=[]
+    AccTest = []
+    for i in tqdm(range(i)): # tqdm permet d'afficher un bare de progression
+        ATrain = Model(XTrain,W,b)
+        if (i%10 == 0): # permte d'aller plus rapitement
+            LogLossTrain.append(Cost(ATrain,yTrain))
+            yPredict = predict(XTrain,W,b)
+            AccTrain.append(accuracy_score(yTrain,yPredict))
+
+            ATest = Model(XTest,W,b)
+            LogLossTest.append(Cost(ATest,yTest))
+            yPredict = predict(XTest,W,b)
+            AccTest.append(accuracy_score(yTest,yPredict))
+        dW,db = Gradiants(ATrain,XTrain,yTrain)
+        W,b = Uptate(W,b,dW,db,a)
+
+    plt.figure(figsize=(12,4))
+    plt.subplot(1,2,1)
+    plt.plot(LogLossTrain, label="Train")
+    plt.plot(LogLossTest, label="Test")
+    plt.legend()
+    plt.subplot(1,2,2)
+    plt.plot(AccTrain, label="Train")
+    plt.plot(AccTest, label="Test")
+    plt.legend()
+    plt.show()
+    return (W,b)
+
